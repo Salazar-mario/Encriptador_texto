7fc85d429e1e65847503bfb42d483a0e9e100a04
agustin salazar <mario.agustin.salazar@gmail.com>
2023-07-29 13:04:05 -0300

    correcion de imagen de fondo y js

--- index.js
+++ index.js
@@ -15,9 +15,7 @@ function encriptar() {
         document.getElementById("texto").value = textoCifrado;
         tituloMensaje.textContent = "Texto encriptado con éxito";
         parrafo.textContent = "";
-        muñeco.src = "./img/encriptado.jpg";
     } else {
-        muñeco.src = "img/404-error.png";
         tituloMensaje.textContent = "Ningún mensaje fue encontrado";
         parrafo.textContent = "Ingresa el texto que deseas encriptar o desencriptar";
         swal("Ooops!", "Debes ingresar un texto", "warning");
@@ -41,9 +39,7 @@ function desencriptar() {
         document.getElementById("texto").value = textoCifrado;
         tituloMensaje.textContent = "Texto desencriptado con éxito";
         parrafo.textContent = "";
-        muñeco.src = "./img/desencriptado.jpg";
     } else {
-        muñeco.src = "./img/muñeco.png";
         tituloMensaje.textContent = "Ningún mensaje fue encontrado";
         parrafo.textContent = "Ingresa el texto que deseas encriptar o desencriptar";
         swal("Ooops!", "Debes ingresar un texto", "warning");
